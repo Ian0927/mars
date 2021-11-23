401b791fdaeebe8ec2b5cbfac6c0c88d9b31d5e9
user <user@host.docker.internal>
2021-11-23 09:38:58 +0900

    init

---
+++ mars/mars_master_config.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# vim configuration 
+echo 'alias vi=vim' >> /etc/profile
+
+# swapoff -a to disable swapping
+swapoff -a
+# sed to comment the swap partition in /etc/fstab
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
+# kubernetes repo
+gg_pkg="packages.cloud.google.com/yum/doc" # Due to shorten addr for key
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://${gg_pkg}/yum-key.gpg https://${gg_pkg}/rpm-package-key.gpg
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+modprobe br_netfilter
+
+# local small dns & vagrant cannot parse and delivery shell code.
+echo "172.27.0.49 mars-master1" >> /etc/hosts
+echo "172.27.0.192 mars-worker1" >> /etc/hosts
+echo "172.27.0.175 mars-worker2" >> /etc/hosts
+
+# config DNS  
+cat <<EOF > /etc/resolv.conf
+nameserver 1.1.1.1 #cloudflare DNS
+nameserver 8.8.8.8 #Google DNS
+EOF
+
+# install packages 
+yum install epel-release -y
+yum install vim-enhanced -y
+yum install git -y
+
+# install docker 
+yum install docker -y && systemctl enable --now docker
+
+# install kubernetes and kubectl will install only master node 
+yum install kubelet-1.18.4 kubeadm-1.18.4 -y
+systemctl enable --now kubelet
+
+#only master node
+# init kubernetes
+kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
+--pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=172.27.0.49
+
+# config for master node only
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+# config for kubernetes's network
+kubectl apply -f \
+https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/172.16_net_calico.yaml
---
+++ mars/mars_worker_config.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# vim configuration 
+echo 'alias vi=vim' >> /etc/profile
+
+# swapoff -a to disable swapping
+swapoff -a
+# sed to comment the swap partition in /etc/fstab
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
+# kubernetes repo
+gg_pkg="packages.cloud.google.com/yum/doc" # Due to shorten addr for key
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://${gg_pkg}/yum-key.gpg https://${gg_pkg}/rpm-package-key.gpg
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+modprobe br_netfilter
+
+# local small dns & vagrant cannot parse and delivery shell code.
+echo "172.27.0.49 mars-master1" >> /etc/hosts
+echo "172.27.0.192 mars-worker1" >> /etc/hosts
+echo "172.27.0.175 mars-worker2" >> /etc/hosts
+
+# config DNS  
+cat <<EOF > /etc/resolv.conf
+nameserver 1.1.1.1 #cloudflare DNS
+nameserver 8.8.8.8 #Google DNS
+EOF
+
+# install packages 
+yum install epel-release -y
+yum install vim-enhanced -y
+yum install git -y
+
+# install docker 
+yum install docker -y && systemctl enable --now docker
+
+# install kubernetes and kubectl will install only master node 
+yum install kubelet-1.18.4 kubeadm-1.18.4 -y
+systemctl enable --now kubelet
+
+#only worker node
+# config for work_nodes only 
+kubeadm join --token 123456.1234567890123456 \
+             --discovery-token-unsafe-skip-ca-verification 172.27.0.49:6443
+
