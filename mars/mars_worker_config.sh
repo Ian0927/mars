7ce601a123de178f821ac0c9779b5be98291c5c3
user <user@host.docker.internal>
2021-11-30 17:34:04 +0900

    worker 서버 추가 mars-worker3

--- mars/mars_worker_config.sh
+++ mars/mars_worker_config.sh
@@ -30,6 +30,7 @@ modprobe br_netfilter
 echo "172.27.0.49 mars-master1" >> /etc/hosts
 echo "172.27.0.192 mars-worker1" >> /etc/hosts
 echo "172.27.0.175 mars-worker2" >> /etc/hosts
+echo "172.27.0.105 mars-worker3" >> /etc/hosts
 
 cat <<EOF > /etc/resolv.conf
 nameserver 1.1.1.1 #cloudflare DNS
