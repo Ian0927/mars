69d7c9e1fd6196ce292e962304fdb6208ce6d2ab
user <user@host.docker.internal>
2021-12-02 09:47:02 +0900

    hosts 추가 스크립트 분리 init

---
+++ mars/mars_add_hosts_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "172.27.0.49 mars-master1" >> /etc/hosts
+echo "172.27.0.192 mars-worker1" >> /etc/hosts
+echo "172.27.0.175 mars-worker2" >> /etc/hosts
+echo "172.27.0.105 mars-worker3" >> /etc/hosts
