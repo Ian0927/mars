df5e513ee5b2550185a7ffb1dbe66eefd383bef7
user <user@host.docker.internal>
2021-12-03 17:55:38 +0900

    yaml URL 변경

--- mars/mars_master_config.sh
+++ mars/mars_master_config.sh
@@ -51,4 +51,4 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://github.com/Ian0927/mars/blob/master/mars/172.27_net_calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/Ian0927/mars/master/mars/172.27_net_calico.yaml
