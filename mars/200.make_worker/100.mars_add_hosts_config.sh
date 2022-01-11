ebe83da9840b4a1b8fc505edb93286269de9e334
user <user@host.docker.internal>
2022-01-11 10:54:57 +0900

    add node's IP

--- mars/200.make_worker/100.mars_add_hosts_config.sh
+++ mars/200.make_worker/100.mars_add_hosts_config.sh
@@ -8,6 +8,8 @@
 # --discovery-token-unsafe-skip-ca-verification
 ####################################################
 echo "172.27.0.49 mars-master1" >> /etc/hosts
-echo "172.27.0.192 mars-worker1" >> /etc/hosts
-echo "172.27.0.175 mars-worker2" >> /etc/hosts
-echo "172.27.0.105 mars-worker3" >> /etc/hosts
+echo "172.27.0.227 mars-worker1" >> /etc/hosts
+echo "172.27.0.228 mars-worker201" >> /etc/hosts
+echo "172.27.0.229 mars-worker202" >> /etc/hosts
+echo "172.27.0.88 mars-worker203" >> /etc/hosts
+echo "172.27.0.27 mars-worker204" >> /etc/hosts
